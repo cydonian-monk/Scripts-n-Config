3ffb979d2695f014b03c8e6b223dd92b7a3dcd2c
root <root@gerty.lan>
2012-04-13 01:27:04 -0500

    Added uptime to tty_sysmon.sh and meminfo to tty10.

--- tty_sysmon.sh
+++ tty_sysmon.sh
@@ -3,7 +3,10 @@
 #
 # Authors: AWC - Andy W. Cummings <cydonian.monk@gmail.com>
 # Revision History:
+#      2012/04/13 AWC Added uptime.
 #      2012/04/12 AWC Initial github copy.
 
+echo "Uptime:" `uptime`
+echo
+echo "Battery Status:"
 cat /proc/acpi/battery/BAT1/state
-
